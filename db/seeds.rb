332f83be95044c849a53e5c60637647d01914c1e
mikoto2000 <mikoto2000@gmail.com>
2023-08-15 23:55:52 +0900

    seed に todo_status の状態を追加。

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+TodoStatus.create!(
+  [
+    {
+      name: "新規"
+    },
+    {
+      name: "進行中"
+    },
+    {
+      name: "完了"
+    }
+  ]
+)
