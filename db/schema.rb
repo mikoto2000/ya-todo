c9b9ca78bb4ab83e40a07f2a75b0855f159bac14
mikoto2000 <mikoto2000@gmail.com>
2023-08-15 23:32:15 +0900

    todo_statuses テーブルの制約を更新。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_14_203543) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_15_141743) do
+  create_table "todo_statuses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_todo_statuses_on_name", unique: true
+  end
+
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
